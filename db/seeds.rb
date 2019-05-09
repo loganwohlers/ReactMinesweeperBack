197f84a6e91e4f2cff5b88c073293a78f15f02c3
clairemuller <clairermuller@gmail.com>
2019-05-09 10:23:50 -0700

    reseed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,12 +12,9 @@ Game.destroy_all
 
 user1 = User.create(username: 'claire')
 user2 = User.create(username: 'logan')
-user3 = User.create(username: 'brian')
-user4 = User.create(username: 'chris')
 
-game1 = Game.create(user: user1, difficulty: 'beginner', score: 20)
-game2 = Game.create(user: user3, difficulty: 'intermediate', score: 30)
-game3 = Game.create(user: user2, difficulty: 'difficult', score: 50)
+game1 = Game.create(user: user2, difficulty: 'beginner', score: 20)
+game2 = Game.create(user: user2, difficulty: 'intermediate', score: 80)
+game3 = Game.create(user: user1, difficulty: 'difficult', score: 200)
 game4 = Game.create(user: user1, difficulty: 'beginner', score: 10)
-game5 = Game.create(user: user4, difficulty: 'intermediate', score: 25)
-game6 = Game.create(user: user2, difficulty: 'intermediate', score: 60)
+game6 = Game.create(user: user2, difficulty: 'intermediate', score: 100)
